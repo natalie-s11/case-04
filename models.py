d0715fa3f76403826a540d9920bd1f34a2084c9c
natalie-s11 <teg9gu@virginia.edu>
2025-09-29 11:15:04 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Literal
 from pydantic import BaseModel, Field, EmailStr, validator
 
 class SurveySubmission(BaseModel):
